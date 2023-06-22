e42c27a16d6d72abef99262284a985c0b506b42a
Prateek Aggarwal <prateekagarwal963@gmail.com>
2023-06-22 13:19:25 +0530

    creating tshirt page

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar/>
+      
       <div>
         <img src='/home.jpg'/>
       </div>
@@ -99,7 +99,7 @@ export default function Home() {
     </div>
   </div>
 </section>
-      <Footer/>
+      
     </>
   )
 }
