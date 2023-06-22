a4418689dbdef389adfe82156548eae631af13e2
Prateek Aggarwal <prateekagarwal963@gmail.com>
2023-06-22 11:26:30 +0530

    add navbar,footer and using tailblock library

---
+++ components/Navbar.js
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+
+const Navbar = () => {
+  return (
+    <div className='flex flex-col md:flex-row md:justify-start justify-center items-center py-2'>
+      <div className='logo mx-5'>
+        <Image src={"/logo.png"} alt='' width={100} height={100} />
+      </div>
+      <div className="nav">
+        <ul className='flex items-center space-x-2 font-bold md:text-xl'>
+          <Link href={'/'}><li>Tshirts</li></Link>
+          <Link href={'/'}><li>Hoodies</li></Link>
+          <Link href={'/'}><li>Stickers</li></Link>
+          <Link href={'/'}><li>Mugs</li></Link>
+        </ul>
+      </div>
+      <div className="cart absolute right-0 top-4 mx-5">
+        <AiOutlineShoppingCart className='text-xl md:text-3xl'/>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
