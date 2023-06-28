ef5bdb3a1f881453a15cbc57889fac522ef9c4fe
Prateek Aggarwal <prateekagarwal963@gmail.com>
2023-06-28 10:52:32 +0530

    adding pincode srviceability

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 const Slug = () => {
   const router = useRouter();
   const { slug } = router.query;
-
+  const [pin, setPin] = useState();
+  const [service, setService] = useState();
+  const checkServiceability = async () => {
+    let pins = await fetch("http://localhost:3000/api/pincode");
+    let pinJson = await pins.json();
+    if (pinJson.includes(parseInt(pin))) {
+      setService(true);
+      console.log("tr");
+    } else {
+      setService(false);
+      console.log("fa");
+    }
+  };
+  const onChangePin = (e) => {
+    setPin(e.target.value);
+  };
   return (
     <>
       <section className="text-gray-600 body-font overflow-hidden">
@@ -16,10 +31,10 @@ const Slug = () => {
             />
             <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
               <h2 className="text-sm title-font text-gray-500 tracking-widest">
-                BRAND NAME
+                CODESWEAR
               </h2>
               <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
-                The Catcher in the Rye
+                Wear the code (XL/Blue)
               </h1>
               <div className="flex mb-4">
                 <span className="flex items-center">
@@ -161,9 +176,12 @@ const Slug = () => {
               </div>
               <div className="flex">
                 <span className="title-font font-medium text-2xl text-gray-900">
-                  $58.00
+                ₹499
                 </span>
-                <button className="flex ml-14 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">
+                <button className="flex ml-8 text-white bg-pink-500 border-0 py-2 px-2 md:px-6 focus:outline-none hover:bg-pink-600 rounded">
+                  Buy Now
+                </button>
+                <button className="flex ml-4 text-white bg-pink-500 border-0 py-2 px-2 md:px-6 focus:outline-none hover:bg-pink-600 rounded">
                   Add To Cart
                 </button>
                 <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
@@ -179,6 +197,26 @@ const Slug = () => {
                   </svg>
                 </button>
               </div>
+              <div className="pin mt-6 flex space-x-2 text-sm">
+                <input
+                  onChange={onChangePin}
+                  className="px-2 border-2 border-gray-400 rounded-md"
+                  type="text"
+                  placeholder="Enter your Pincode"
+                />
+                <button
+                  onClick={checkServiceability}
+                  className="text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded"
+                >
+                  Check
+                </button>
+              </div>
+             {(!service && service!=null) && <div className="text-red-700 text-sm mt-3">
+                Sorry! We do not deliver to this pin
+              </div>}
+              {(service && service!=null) && <div className="text-green-700 text-sm mt-3">
+                Yay! This pincode is servicable
+              </div>}
             </div>
           </div>
         </div>
