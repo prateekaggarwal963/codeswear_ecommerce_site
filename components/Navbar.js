174d09dcd0d1885f3a781f8335e708f089fb651e
Prateek Aggarwal <prateekagarwal963@gmail.com>
2023-06-28 09:28:29 +0530

    Designing Product Page

--- components/Navbar.js
+++ components/Navbar.js
@@ -73,10 +73,15 @@ const Navbar = () => {
             </div>
           </li>
         </ol>
-        <button className="flex mx-auto mt-16 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-sm">
+        <div className="flex">
+        <button className="flex mr-2 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded text-sm">
           <BsFillBagCheckFill className="m-1" />
           Checkout
         </button>
+        <button className="flex mr-2 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded text-sm">
+          Clear Cart
+        </button>
+        </div>
       </div>
     </div>
   );
